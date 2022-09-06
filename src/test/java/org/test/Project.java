94a808f97e2cd50edb740a4f2b1faee22a69dad9
swarna9944 <swarnalathas1997@gmail.com>
2022-09-06 11:43:50 -0700

    swarna's code

--- src/test/java/org/test/Project.java
+++ src/test/java/org/test/Project.java
@@ -2,9 +2,9 @@ package org.test;
 
 public class Project {
 public static void main(String[] args) {
-	System.out.println("s1");
-	System.out.println("s2");
-	System.out.println("s3");
+	System.out.println("software1");
+	System.out.println("software2");
+	System.out.println("software3");
 	
 }
 }
