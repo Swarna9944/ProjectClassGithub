6cf009fc7adcae530d0c8e56f765eb4d5d366d79
swarna9944 <swarnalathas1997@gmail.com>
2022-09-06 12:15:11 -0700

    Latha's code

--- src/test/java/org/test/Project.java
+++ src/test/java/org/test/Project.java
@@ -1,5 +1,10 @@
 package org.test;
 
 public class Project {
-
+public static void main(String[] args) {
+	System.out.println("software1");
+	System.out.println("software4");
+	System.out.println("software5");
+	
+}
 }
