1b26d63c9bf9acb9f649fdaf30198febf5ee1555
swarna9944 <swarnalathas1997@gmail.com>
2022-09-07 13:14:50 -0700

    new swarna's code

--- src/test/java/org/test/Project.java
+++ src/test/java/org/test/Project.java
@@ -7,6 +7,6 @@ public static void main(String[] args) {
 	System.out.println("software3");
 	System.out.println("software4");
 	System.out.println("software5");
-	
+	System.out.println("software6");
 }
 }
